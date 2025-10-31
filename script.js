22f02a0e10ab6d242f74f40ca917c60796aac135
@mars0 <marti3014soria@gmail.com>
2025-10-31 02:24:32 -0300

    buscador y panel desplegable

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const iconoMovil = L.icon({
 // Marcadores de centros fijos
 centrosFijos.forEach((c) => {
   const marker = L.marker(c.coords, { icon: iconoFijo }).bindPopup(
-    `<b>${c.nombre}</b><br>${c.detalle}<br>${c.horario}`
+    `<b>${c.nombre}</b>`
   ); // Popup con detalle del centro
   marker.tipo = "fijo";
   marker.nombre = c.nombre;
@@ -41,7 +41,7 @@ centrosFijos.forEach((c) => {
 // Marcadores de centros móviles
 centrosMoviles.forEach((m) => {
   const marker = L.marker(m.coords, { icon: iconoMovil }).bindPopup(
-    `<b>${m.nombre}</b><br>${m.detalle}<br>${m.horario}`
+    `<b>${m.nombre}</b>`
   );
   marker.tipo = "movil";
   marker.nombre = m.nombre;
@@ -59,11 +59,43 @@ const lista = document.getElementById("lista-centros");
 
 const renderLista = (items) => {
   lista.innerHTML = "";
+
   listaActual = items;
   items.forEach((item, i) => {
     const li = document.createElement("li");
     li.textContent = item.nombre;
+
+    const seccionInfo = document.createElement("div");
+    seccionInfo.classList.add("infoCentro");
+
+    const infoDetalle = document.createElement("span")
+    infoDetalle.textContent = item.detalle;
+
+    const separar = document.createElement("br");
+
+    const infoHorario = document.createElement("span");
+    infoHorario.textContent = item.horario;
+    
+    const botonUbi = document.createElement("div");
+    botonUbi.classList.add("botonUbicacion");
+
+    const infoUbicacion= document.createElement("button");
+    infoUbicacion.classList.add("verUbicacion");
+    infoUbicacion.textContent = "Ver Ubicacion";
+
+    seccionInfo.appendChild(infoDetalle);
+    seccionInfo.appendChild(separar);
+    seccionInfo.appendChild(infoHorario);
+    seccionInfo.appendChild(botonUbi);
+    botonUbi.appendChild(infoUbicacion);
+
+    li.appendChild(seccionInfo);
+
     li.addEventListener("click", () => seleccionarCentro(item, i));
+    infoUbicacion.addEventListener("click", (e) =>{
+                                    e.stopPropagation(); 
+                                    botonUbicacion(item,i)});
+                                    
     lista.appendChild(li);
   });
 };
@@ -72,10 +104,24 @@ renderLista([...centrosFijos, ...centrosMoviles]);
 
 // --- SELECCION DEL CENTRO DESDE PANEL LATERAL ---
 const seleccionarCentro = (centro, index) => {
-  // Centrar mapa
-  map.setView(centro.coords, 15);
 
-  // Abrir popup
+  // Seteo en el panel el centro seleccionado
+  document
+    .querySelectorAll("#lista-centros li")
+    .forEach((li) => {
+      li.classList.remove("activo")
+    });
+  document.querySelectorAll("#lista-centros li")[index].classList.toggle("activo");
+  document.querySelectorAll("#lista-centros li")[index].classList.toggle("expande");
+  document
+    .querySelectorAll(".filtros button")
+    .forEach((b) => b.classList.remove("activo"));
+
+};
+
+// --- BOTON PARA VER LA UBICACION DEL CENTRO ---
+const botonUbicacion = (centro, index) => {
+  map.setView(centro.coords,15);
   const marker = marcadores.find(
     (m) =>
       m.getLatLng().lat === centro.coords[0] &&
@@ -84,17 +130,8 @@ const seleccionarCentro = (centro, index) => {
   if (marker) {
     marker.openPopup();
   }
-
-  // Seteo en el panel el centro seleccionado
-  document
-    .querySelectorAll("#lista-centros li")
-    .forEach((li) => li.classList.remove("activo"));
   document.querySelectorAll("#lista-centros li")[index].classList.add("activo");
-
-  document
-    .querySelectorAll(".filtros button")
-    .forEach((b) => b.classList.remove("activo"));
-};
+}
 
 // --- SELECCION DESDE EL MAPA ---
 marcadores.forEach((marker) => {
@@ -106,7 +143,8 @@ marcadores.forEach((marker) => {
         .forEach((li) => li.classList.remove("activo"));
       const liSeleccionado =
         document.querySelectorAll("#lista-centros li")[index];
-      liSeleccionado.classList.add("activo");
+      liSeleccionado.classList.toggle("activo");
+      liSeleccionado.classList.toggle("expande");
       liSeleccionado.scrollIntoView({ behavior: "smooth", block: "center" });
     }
 
@@ -116,6 +154,19 @@ marcadores.forEach((marker) => {
   });
 });
 
+// --- BUSCADOR ---
+const busqueda = () =>{
+  const centroIngresado = document.getElementById("busquedaCentro");
+  const todosLosCentros=[...centrosFijos, ...centrosMoviles];
+  const centroElegido= centroIngresado.value.trim();
+
+  const centrosCoincidentesBusqueda = todosLosCentros.filter(c => {
+    return c.nombre.toLowerCase().includes(centroElegido.toLowerCase());
+  })
+  renderLista(centrosCoincidentesBusqueda);
+  map.addLayer(centrosCoincidentesBusqueda);
+
+}
 // --- BOTONES FILTROS ---
 const mostrarFijos = () => {
   map.removeLayer(grupoMoviles);
@@ -142,6 +193,7 @@ const mostrarTodos = () => {
 }
 
 // --- EVENTOS DE BOTONES ---
+document.getElementById("buscar").addEventListener("click", busqueda);
 document.getElementById("btnFijos").addEventListener("click", mostrarFijos);
 document.getElementById("btnMoviles").addEventListener("click", mostrarMoviles);
 document.getElementById("btnTodos").addEventListener("click", mostrarTodos);
